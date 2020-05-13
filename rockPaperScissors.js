05e5c9ecc224688683c075154b35f7f81133879e
Jed Boyle <jedboyle01@gmail.com>
2020-05-13 13:17:21 +0100

    002 - Since a user can pass in a parameter, such as ‘Rock’ or ‘rock’ with different capitalizations, begin by utilizing JavaScript’s toLowerCase() function to make the userInput all lowercase.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,4 +1,5 @@
 // 001 - Using const and arrow function syntax, create a function named getUserChoice that takes a single parameter userInput.
 const getUserChoice = (userInput) => {
-
+// 002 - Since a user can pass in a parameter, such as ‘Rock’ or ‘rock’ with different capitalizations, begin by utilizing JavaScript’s toLowerCase() function to make the userInput all lowercase.
+userInput = userInput.toLowerCase();
 };
